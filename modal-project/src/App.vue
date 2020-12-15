6092f20d246e004b84678bbebb8eca02e3a6a77a
Mourad EL CADI <mouradface1998@gmail.com>
2020-12-15 23:37:43 +0100

    getters and setters

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -22,6 +22,11 @@
   </modal>
 
   <cookie v-if="showCookieMsg" @closeCookie="showCookieMsg=false" />
+  <hr>
+  <div>{{ fullname }}</div>
+  <input type="text" placeholder="fname" v-model="fname">
+  <input type="text" placeholder="lname" v-model="lname">
+  <button @click="setFullName">Set Full Name</button>
   
 </template>
 
@@ -39,6 +44,22 @@ export default {
     cookie,
   },
 
+  computed: {
+    // fullname(){
+    //   return 'The full name is ' + this.fname + ' ' + this.lname
+    // },
+
+    fullname: {
+      get(){
+        return 'The full name is ' + this.fname + ' ' + this.lname
+      },
+
+      set(){
+        alert('hello')
+      }
+    },
+  },
+
   data(){
     return {
       popmessage:'some random shit goes here',
@@ -46,14 +67,20 @@ export default {
       poptitle:'The Pop Up title',
       pophead:'The Pop Up head',
       visibleModal:false,
-      showCookieMsg:false
-    }
+      showCookieMsg:false,
+      fname: null,
+      lname: null
+      }
   },
 
   methods: {
     handle() {
       console.log(this.$refs.name)
       this.$refs.name.classList.toggle('foo')
+    },
+
+    setFullName(){
+      this.fullname = 'Mourad EL CADI'
     }
   }
 }
@@ -73,12 +100,12 @@ export default {
   padding: 0;
 }
 
-.foo{
+.foo {
   padding: 2rem;
   background-color: red;
 }
 
-button{
+button {
   margin: 1em;
   border: none;
   background-color: blue;
